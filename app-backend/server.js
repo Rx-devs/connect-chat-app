838808e97c35b29dbea12a48a635473b353d6294
Tonmoy Roy <rxdevs100@gmail.com>
2022-05-01 18:38:58 +0600

    backend updated with demo

--- app-backend/server.js
+++ app-backend/server.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
 
 const app = express();
+dotenv.config();
 
 app.get('/', (req, res) => {
-    res.send("API is running");
+    res.send("API is running successfully");
 });
 
+app.get('/api/chat', (req, res) => {
+    res.send(chats);
+});
 
-app.listen(5000, console.log("Server started on port 5000"));
+app.get('/api/chat/:id', (req, res) => {
+    // console.log(req.params.id);
+    const singleChat = chats.find((c) => c._id === req.params.id);
+    res.send(singleChat);
+});
+
+const PORT = process.env.PORT;
+app.listen(PORT, console.log(`Server started on port ${PORT}`
+));
 
